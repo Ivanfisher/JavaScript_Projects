83ff6db1f9953b5650ee0204ab8dc65e4da019c2
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-08-18 12:42:22 -0400

    Added Folder

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert("This is only a practice page."); // Utilizing the window.alert() method.
+var firstName = "Ivan ";
+var lastName = "Fisher ";
+var name = firstName + lastName; // Concatenating two string variables 
+var name = name.fontcolor("#444"); // Giving name a dark gray color
+document.write("Hello, my name is " + name + "and I am "); // Utilizing document.write
+document.write(20 + 4); // Writing an expression
+document.write(" years old.");
