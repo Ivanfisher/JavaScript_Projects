3d5b1ef6defffbe78d25c232b60d8cd4b1b4af8a
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-08-18 13:52:42 -0400

    Added Folder

--- Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -6,3 +6,8 @@ var name = name.fontcolor("#444"); // Giving name a dark gray color
 document.write("Hello, my name is " + name + "and I am "); // Utilizing document.write
 document.write(20 + 4); // Writing an expression
 document.write(" years old.");
+function Turn_Blue() {
+    var blue = "This should be able to turn blue.";
+    var result = blue.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
---
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,6 @@
+function concatenate() {
+    var str = "BOO! ";
+    str += "Scared you."; // Concatenating two strings
+    var result = str.fontcolor("red");
+    document.getElementById("Boo").innerHTML = result;
+}
