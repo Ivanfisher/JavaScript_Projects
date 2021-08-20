c5dc2e982c880e227b3faedda97e2d47d1f414f2
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-08-20 11:12:38 -0400

    Added comments in JavaScript code

--- Basic_JavaScript_Projects/Project7_Scope_Time_Function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_Scope_Time_Function/JS/main.js
@@ -1,15 +1,19 @@
+// Using local variable
 function subtraction() {
     var x = 34;
     document.getElementById("subtract").innerHTML = (x - 12);
 }
 
+// Utilizing global variable
 var z = 25;
 function multiplication() {
     document.getElementById("multiply").innerHTML = (z * 3);
 }
 
+// Utilizing console.log to debug an intentionally errored code "using an undefined variable"
 console.log(x + 31);
 
+// Utilizing Date .getHours and conditional statements to determine output
 function Time_function() {
     var time = new Date().getHours();
     var reply;
