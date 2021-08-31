da435078f4ef659d3c85d6d00c136fe2a3837ec7
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-08-31 14:32:23 -0400

    Updated Files

--- Basic_JavaScript_Projects/Project2_Functions/JS/main.js
+++
@@ -1,6 +0,0 @@
-function concatenate() {
-    var str = "BOO! ";
-    str += "Scared you."; // Concatenating two strings
-    var result = str.fontcolor("red");
-    document.getElementById("Boo").innerHTML = result;
-}
--- JavaScript_Projects/JavaScript/javascript_3.js
+++
@@ -1,4 +0,0 @@
-function symbols(element) {
-    var elementIs = element.getAttribute("data-element-symbol");
-    alert(element.innerHTML + " is the symbol for the element " + elementIs + ".");
-}
