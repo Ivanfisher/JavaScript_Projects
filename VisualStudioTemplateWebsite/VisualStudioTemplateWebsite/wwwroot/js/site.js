60facebe68f6180348a831530df9d958e0ca457a
Ivan Fisher <87035839+Ivanfisher@users.noreply.github.com>
2021-09-07 14:30:41 -0400

    Added Folder
    Added a basic website page using a template provided by visual studio.

---
+++ VisualStudioTemplateWebsite/VisualStudioTemplateWebsite/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 8);
+    function frame() {
+        if (pos == 200) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+        }
+    }
+}
